02898284cdb0ad004bd292a53bbd3f9218dc86b1
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-24 22:34:12 +0700

    initialize project for starter-code

--- app/src/main/java/com/exfarnanda1945/netdemymvvm/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/netdemymvvm/MainActivity.kt
@@ -2,10 +2,47 @@ package com.exfarnanda1945.netdemymvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.exfarnanda1945.netdemymvvm.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val api by lazy {
+        ApiConfig.api
+    }
+    private val rvAdapter by lazy {
+        NewsAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val rv = binding.rvNews
+
+        api.listNews().enqueue(object : Callback<NewsResponse> {
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
+                if (response.body() != null) {
+                    rvAdapter.setData(response.body()?.articles!!)
+                    rv.apply {
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = rvAdapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.d("Error", "onFailure: ${t.localizedMessage}")
+                Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
     }
 }
